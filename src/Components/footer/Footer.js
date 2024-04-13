b558cef2581c4f4263e4301fd01727016633f035
Ram Harsh Pandey <120307005+ram-ethan@users.noreply.github.com>
2024-04-13 12:06:57 +0530

    Update Footer.js

--- src/Components/footer/Footer.js
+++ src/Components/footer/Footer.js
@@ -3,7 +3,7 @@ import './footer.css'
 export default function Footer() {
     return (
         <div className='footer container-fluid p-3 d-flex align-items-center justify-content-center text-center'>
-            <h4>todo</h4> &nbsp; <p className='m-0 '>&copy;TheCODEMASTER</p>
+            <h4>todo</h4> &nbsp; <p className='m-0 '>&copy;ram-ethan</p>
         </div>
     )
 }
